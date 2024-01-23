edb24c3ef2c29b2eebbbfb45320c68e83409013f
Matteo Marchegiani <matmarcheg@gmail.com>
2024-01-23 14:05:13 +0100

    Update job scripts

--- jobs/job_script.sh
+++ jobs/job_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
+NUM_GPU=1
 cd /opt/SPANet
-python -m spanet.train -of /afs/cern.ch/user/y/ymaidann/options_files/tth.json --epochs 50 --gpus 1 -l /afs/cern.ch/user/y/ymaidann/
+python -m spanet.train -of  /afs/cern.ch/work/m/mmarcheg/ttHbb/SPANet/options_files/full_hadronic_ttbar/example.json --time_limit 00:00:01:00 --gpus $NUM_GPU
 

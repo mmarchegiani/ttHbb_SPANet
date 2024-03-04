18de0ad775eef9305f5e8826d20d52e9ade7986f
Matteo Marchegiani <matmarcheg@gmail.com>
2024-03-04 14:09:45 +0100

    Updated condor job submission script

--- scripts/submit_to_condor.py
+++ scripts/submit_to_condor.py
@@ -1,13 +1,16 @@
-import htcondor
-import sys
-import argparse
 import os
-import yaml
+import argparse
+
+import htcondor
+
+from omegaconf import OmegaConf
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, required=True)
-parser.add_argument('--version', type=str, required=True)
-parser.add_argument('--path-configFile', type=str, required=True)
+parser.add_argument('--cfg', type=str, required=True)
+parser.add_argument("-of", "--options_file", type=str, default=None,
+                    help="JSON file with option overloads.", required=True)
+parser.add_argument("-l", "--log_dir", type=str, default=None,
+                    help="Output directory for the checkpoints and tensorboard logs. Default to current directory.", required=True)
 parser.add_argument('--dry', action="store_true")
 parser.add_argument('--interactive', action="store_true")
 parser.add_argument('--ngpu', type=int, default=1)
@@ -16,61 +19,55 @@ parser.add_argument("--good-gpus", action="store_true")
 parser.add_argument("--args", nargs="+", type=str, help="additional args")
 args = parser.parse_args()
 
-model = args.model
-version = args.version
-dry = args.dry
 interactive = args.interactive
-path_to_conf = args.path_configFile
 
 col = htcondor.Collector()
 credd = htcondor.Credd()
 credd.add_user_cred(htcondor.CredTypes.Kerberos, None)
 
-# Read config file in 'conf'
-with open(path_to_conf) as f:
-    try:
-        conf = yaml.safe_load(f)
-    except yaml.YAMLError as exc:
-        print(exc)
+cfg = OmegaConf.load(args.cfg)
+basedir = cfg['path']
+model = cfg['model']
+ngpu = cfg['ngpu']
+ncpu = cfg['ncpu']
 
-print("Path to root & outputDir: \n", conf)
+# Override defaults by command line arguments
+if args.ngpu != parser.get_default("ngpu"):
+    ngpu = args.ngpu
+if args.ncpu != parser.get_default("ncpu"):
+    ncpu = args.ncpu
 
-basedir = conf['path_to_root']
-outputDir = conf['path_to_outputDir']
-
+print("Initializing job submission script...", end="\n\n")
 sub = htcondor.Submit()
 
 if interactive:
     sub['InteractiveJob'] = True
 
-if model == "huber_mmd":
-    sub['Executable'] = f"{basedir}/jobs/script_condor_pretraining_huber_mmd.sh"
-    sub['Error'] = f"{basedir}/jobs/error/huber-mmd-$(ClusterId).$(ProcId).err"
-    sub['Output'] = f"{basedir}/jobs/output/huber-mmd-$(ClusterId).$(Proc1Id).out"
-    sub['Log'] = f"{basedir}/jobs/log/huber-mmd-$(ClusterId).log"
+if model == "jet_assignment":
+    sub['Executable'] = f"{basedir}/jobs/jet_assignment.sh"
+    sub['arguments'] = f"{args.options_file} {args.log_dir}"
+    sub['Output'] = f"{basedir}/jobs/output/jet_assignment-$(ClusterId).$(Proc1Id).out"
+    sub['Error'] = f"{basedir}/jobs/error/jet_assignment-$(ClusterId).$(ProcId).err"
+    sub['Log'] = f"{basedir}/jobs/log/jet_assignment-$(ClusterId).log"
     sub['MY.SendCredential'] = True
-    sub['MY.SingularityImage'] = '"/cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/dvalsecc/memflow:latest"'
+    sub['MY.SingularityImage'] = '"/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmsml/cmsml:latest"'
     sub['+JobFlavour'] = '"nextweek"'
-    sub['arguments'] = f"{basedir} configs/pretraining_huber_mmd/pretraining_huber_mmd_v{version}.yaml {outputDir}/flow_pretraining_huber_mmd"
 else:
     raise ValueError(f"Model {model} not implemented")
 
 # General
 sub['request_cpus'] = f"{args.ncpu}"
 sub['request_gpus'] = f"{args.ngpu}"
+
 if args.good_gpus:
     sub['requirements'] = 'regexp("A100", TARGET.CUDADeviceName) || regexp("V100", TARGET.CUDADeviceName)'
 
-print(f"{basedir}/{sub['arguments'].split()[1]}")
-
-if model != "flow_evaluation_labframe" and not os.path.exists(f"{basedir}/{sub['arguments'].split()[1]}"):
-    print("Missing configuration file! The jobs has not been submitted")
-    exit(1)
-    
+print("Submission parameters:")
 print(sub)
-if not dry:
-    schedd = htcondor.Schedd()
-    with schedd.transaction() as txn:
-        cluster_id = sub.queue(txn)
+ 
+if not args.dry:
+    print("Starting Condor scheduler...")
+    client = htcondor.Schedd()
+    result = client.submit(sub, count=1)
 
-    print(f"Submitted to {cluster_id:=}")
+    print(f"Submitted {result.num_procs()} job(s) to {result.cluster()}")

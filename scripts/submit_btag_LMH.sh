8a59db46d88563bcb478782b36100e02cb3d4874
Matteo Marchegiani <matmarcheg@gmail.com>
2024-03-05 15:06:44 +0100

    Script to submit btag_LMH

---
+++ scripts/submit_btag_LMH.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python scripts/submit_to_condor.py --cfg jobs/config/jet_assignment.yaml -of options_files/ttHbb_semileptonic/options_full_Run2_btag_LMH.json -l /eos/user/m/mmarcheg/ttHbb/models/jet_assignment_full_Run2_btag_LMH

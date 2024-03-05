375200172a32b7334bb8034920453a93e181ad51
Matteo Marchegiani <matmarcheg@gmail.com>
2024-03-05 23:03:33 +0100

    Refactor H5 dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,12 +1,14 @@
 import os
 from collections import defaultdict
 
+import numba
 import vector
 vector.register_numba()
 vector.register_awkward()
 
 import numpy as np
 import awkward as ak
+import h5py
 
 from omegaconf import OmegaConf
 
@@ -159,21 +161,239 @@ class ParquetDataset:
             ak.to_parquet(df_out, self.output_file)
 
 class H5Dataset:
-    def __init__(self, input, output_file, cfg, cat="semilep_LHE"):
+    def __init__(self, input_file, output_file, cfg, fully_matched=False):
         # Load several input files into a list
-        if type(input) == str:
-            self.input_files = [input]
-        elif type(input) == list:
-            self.input_files = input
+        if type(input_file) == str:
+            self.input_files = [input_file]
+        elif type(input_file) == list:
+            self.input_files = input_file
+        else:
+            raise ValueError(f"Input file {input_file} should be a string or a list of strings.")
         self.output_file = output_file
         self.cfg = cfg
-        self.cat = cat
+        self.fully_matched = fully_matched
 
-        self.load_input()
         self.load_config()
         self.check_output()
-        self.load_features()
+        self.dataset = Dataset(self)
+        if self.fully_matched:
+            self.select_fully_matched()
 
     def load_input(self):
         '''Load the input file.'''
+        # Check if the parquet files exist
+        for input_file in self.input_files:
+            if not os.path.exists(input_file):
+                raise ValueError(f"Input file {input_file} does not exist.")
+            if not input_file.endswith(".parquet"):
+                raise ValueError(f"Input file {input_file} should have the `.parquet` extension.")
+        # Read the parquet files
+        print(f"Reading {len(self.input_files)} parquet files: ", self.input_files)
+        dfs = []
+        for input_file in self.input_files:
+            print("Reading file: ", input_file)
+            df = ak.from_parquet(input_file)
+            dfs.append(df)
+        # Merge the dataframes into a single dataframe
+        return ak.concatenate(dfs)
+
+    def load_config(self):
+        '''Load the config file with OmegaConf and read the input features.'''
+        self.cfg = OmegaConf.load(self.cfg)
+        self.input_features = self.cfg["input"]
+        self.collection = self.cfg["collection"]
+        self.targets = self.cfg["particles"]
+        self.frac_train = self.cfg["frac_train"]
+
+    def check_output(self):
+        '''Check the output file extension and if it already exists.'''
+        # Check the output file extension
+        filename, file_extension = os.path.splitext(self.output_file)
+        if not file_extension == ".h5":
+            raise ValueError(f"Output file {self.output_file} should be in .h5 format.")
+        # Check if output file exists
+        if os.path.exists(self.output_file):
+            raise ValueError(f"Output file {self.output_file} already exists.")
+        os.makedirs(os.path.abspath(os.path.dirname(self.output_file)), exist_ok=True)
+
+    def select_fully_matched(self):
+        '''Select only fully matched events.'''
+        mask_fullymatched = ak.sum(self.dataset.df[self.collection["Jet"]].matched == True, axis=1) >= 6
+        df = self.dataset.df[mask_fullymatched]
+        jets = df[self.collection["Jet"]]
+
+        # We require exactly 2 jets from the Higgs, 3 jets from the W or hadronic top, and 1 lepton from the leptonic top
+        jets_higgs = jets[jets.prov == 1]
+        mask_match = ak.num(jets_higgs) == 2
+
+        jets_w_thadr = jets[(jets.prov == 5) | (jets.prov == 2)]
+        mask_match = mask_match & (ak.num(jets_w_thadr) == 3)
+
+        jets_tlep = jets[jets.prov == 3]
+        mask_match = mask_match & (ak.num(jets_tlep) == 1)
+
+        df = df[mask_match]
+        print(f"Selected {len(df)} fully matched events")
+
+        self.dataset.df = df
+
+    def create_groups(self):
+        '''Create the groups in the h5 file.'''
+        for target in self.targets:
+            self.file.create_group(f"TARGET/{target}")
+        for object in self.input_features.keys():
+            self.file.create_group(f"INPUTS/{object}")
+
+    def create_targets(self, df):
+        jets = df[self.collection["Jet"]]
+        indices = ak.local_index(jets)
+
+        for target in self.targets:
+            if target == "h":
+                mask = jets.prov == 1 # H->b1b2
+                # We select the local indices of jets matched with the Higgs
+                # The indices are padded with None such that there are 2 entries per event
+                # The None values are filled with -1 (a nan value).
+                indices_prov = ak.fill_none(ak.pad_none(indices[mask], 2), -1)
+
+                index_b1 = indices_prov[:,0]
+                index_b2 = indices_prov[:,1]
+
+                self.file.create_dataset(f"TARGET/h/b1", np.shape(index_b1), dtype='int64', data=index_b1)
+                self.file.create_dataset(f"TARGET/h/b2", np.shape(index_b2), dtype='int64', data=index_b2)
+
+            elif target == "t1":
+                mask = jets.prov == 5 # W->q1q2 from t1
+                indices_prov = ak.fill_none(ak.pad_none(indices[mask], 2), -1)
+
+                index_q1 = indices_prov[:,0]
+                index_q2 = indices_prov[:,1]
+
+                mask = jets.prov == 2 # t1->Wb
+                index_b_hadr = ak.fill_none(ak.pad_none(indices[mask], 1), -1)[:,0]
+
+                self.file.create_dataset(f"TARGET/t1/q1", np.shape(index_q1), dtype='int64', data=index_q1)
+                self.file.create_dataset(f"TARGET/t1/q2", np.shape(index_q2), dtype='int64', data=index_q2)
+                self.file.create_dataset(f"TARGET/t1/b", np.shape(index_b_hadr), dtype='int64', data=index_b_hadr)
+
+            elif target == "t2":
+                mask = jets.prov == 3 # t2->b
+                index_b_lep = ak.fill_none(ak.pad_none(indices[mask], 1), -1)[:,0]
+
+                self.file.create_dataset(f"TARGET/t2/b", np.shape(index_b_lep), dtype='int64', data=index_b_lep)
+
+    def create_inputs(self, df):
+        '''Create the input arrays in the h5 file.'''
+        features = self.get_object_features(df)
+
+        for obj, feats in features.items():
+            for feat, val in feats.items():
+                if feat == "MASK":
+                    dtype = 'bool'
+                else:
+                    dtype = 'float32'
+                dataset_name = f"INPUTS/{obj}/{feat}"
+                print("Creating dataset: ", dataset_name)
+                ds = self.file.create_dataset(dataset_name, np.shape(val), dtype=dtype, data=val)
+
+    def get_object_features(self, df):
+
+        df_features = defaultdict(dict)
+        for obj, feats in self.input_features.items():
+
+            features = self.input_features[obj]
+            if obj == "Event":
+                if "ht" in features:
+                    features_dict["ht"] = ak.sum(df["JetGood"]["pt"], axis=1)
+                else:
+                    raise NotImplementedError
+            else:
+                collection = self.collection[obj]
+
+                if (collection in df.fields) & (collection != "Event"):
+                    objects = df[collection]
+                    features_dict = {}
+                    for feat in features:
+                        if feat in ["MASK", "ht"]: continue
+                        if feat in ["sin_phi", "cos_phi"]:
+                            phi = objects["phi"]
+                            if feat == "sin_phi":
+                                values = np.sin(phi)
+                            elif feat == "cos_phi":
+                                values = np.cos(phi)
+                        elif feat == "is_electron":
+                            values = ak.values_astype(abs(objects["pdgId"]) == 11, int)
+                        else:
+                            values = objects[feat]
+                        if objects.ndim == 1:
+                            features_dict[feat] = ak.to_numpy(values)
+                        elif objects.ndim == 2:
+                            features_dict[feat] = ak.to_numpy(ak.fill_none(ak.pad_none(values, 16, clip=True), 0))
+                        else:
+                            raise NotImplementedError
+
+                    if "MASK" in features:
+                        if not "pt" in features:
+                            raise NotImplementedError
+                        features_dict["MASK"] = ~(features_dict["pt"] == 0)
+            df_features[obj] = features_dict
+        return df_features
+
+    def h5_tree(self, val, pre=''):
+        items = len(val)
+        for key, val in val.items():
+            items -= 1
+            if items == 0:
+                # the last item
+                if type(val) == h5py._hl.group.Group:
+                    print(pre + '└── ' + key)
+                    self.h5_tree(val, pre+'    ')
+                else:
+                    print(pre + '└── ' + key + ' (%d)' % len(val))
+            else:
+                if type(val) == h5py._hl.group.Group:
+                    print(pre + '├── ' + key)
+                    self.h5_tree(val, pre+'│   ')
+                else:
+                    print(pre + '├── ' + key + ' (%d)' % len(val))
+
+    def print(self):
+        '''Print the h5 file tree.'''
+        self.h5_tree(self.file)
+
+    def save_h5(self, dataset_type):
+        '''Save the h5 file.'''
+        assert dataset_type in ["train", "test"], f"Dataset type `{dataset_type}` not recognized."
+
+        df = getattr(self.dataset, dataset_type)
+        output_file = self.output_file.replace(".h5", f"_{dataset_type}_{len(df)}.h5")
         
+        print("Creating output file: ", output_file)
+        self.file = h5py.File(output_file, "w")
+        self.create_groups()
+        self.create_targets(df)
+        self.create_inputs(df)
+        print(self.file)
+        self.print()
+        self.file.close()
+
+    def save_h5_all(self):
+        '''Save the h5 file for both the training and testing datasets.'''
+        for dataset in ["train", "test"]:
+            print(f"Processing dataset: {dataset} ({len(getattr(self.dataset, dataset))} events)")
+            self.save_h5(dataset)
+
+class Dataset:
+    def __init__(self, h5_dataset : H5Dataset):
+        self.df = h5_dataset.load_input()
+        self.frac_train = h5_dataset.frac_train
+
+    @property
+    def train(self):
+        '''Return the training dataset according to the fraction `frac_train`.'''
+        return self.df[:int(self.frac_train*len(self.df))]
+
+    @property
+    def test(self):
+        '''Return the testing dataset according to the fraction `frac_train`.'''
+        return self.df[int(self.frac_train*len(self.df)):]

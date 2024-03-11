643e3037450b9748a22445e1dace14d737fc9fbb
Matteo Marchegiani <matmarcheg@gmail.com>
2024-03-11 14:16:19 +0100

    Update jobs submission scripts

--- scripts/submit_classification_2017.sh
+++ scripts/submit_classification_2017.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python scripts/submit_to_condor.py --cfg jobs/config/classification.yaml -of options_files/ttHbb_semileptonic/options_full_Run2_classifier.json -l /eos/user/m/mmarcheg/ttHbb/models/classifier_2017 --good-gpus
+python scripts/submit_to_condor.py --cfg jobs/config/classification.yaml -of options_files/ttHbb_semileptonic/classifier/options_full_Run2_classifier.json -l /eos/user/m/mmarcheg/ttHbb/models/classifier_2017
+python scripts/submit_to_condor.py --cfg jobs/config/classification.yaml -of options_files/ttHbb_semileptonic/classifier/options_full_Run2_classifier_medium_size.json -l /eos/user/m/mmarcheg/ttHbb/models/classifier_2017

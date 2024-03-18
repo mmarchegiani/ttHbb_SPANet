a79d979a35937227485c46a1f67716d7c06a94ab
mmarchegiani <matmarcheg@gmail.com>
2024-03-18 13:15:57 +0100

    Update package requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="ttHbb_SPANet",
     packages=find_packages(),
-    #version="2.2.0",
+    version="0.1.0",
     description="Repository for development of a signal vs background classifier in the ttH(bb) analysis based on SPANet.",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -16,21 +16,13 @@ setup(
     url="https://github.com/mmarchegiani/ttHbb_SPANet",
     #license="BSD-3-Clause",
     install_requires=[
-        'torch>=2.0',
-        'numpy>=1.24',
+        'coffea==0.7.22',
         'h5py>=3.9',
-        'numba>=0.57',
-        'pytorch-lightning>=2.0',
-        'tqdm',
-        'sympy',
-        'pyyaml',
-        'opt_einsum',
-        'scikit-learn',
-        'rich',
-        'htcondor'
+        'vector',
+        'omegaconf'
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 1 - Planning',
         'Environment :: Console',
         'Environment :: GPU :: NVIDIA CUDA :: 11.7',
         'Environment :: GPU :: NVIDIA CUDA :: 11.8',

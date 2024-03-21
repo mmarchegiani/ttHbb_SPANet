e7e08d442aeeaef69ddb324b0d5b05d97097612f
Matteo Marchegiani <matmarcheg@gmail.com>
2024-03-21 08:37:53 +0100

    Script to create the h5 datasets for multiclassifier

--- scripts/create_h5_datasets_full_Run2_classifier.sh
+++ scripts/create_h5_datasets_full_Run2_classifier.sh
@@ -8,13 +8,13 @@ input_files=(
     "/eos/user/m/mmarcheg/ttHbb/training_datasets/2018/output_TTToSemiLeptonic_2018_TTToSemiLeptonic__TTToSemiLeptonic_tt+LF.parquet"
 )
 
-output_file="/eos/user/m/mmarcheg/ttHbb/training_datasets/classifier/multiclassifier_2018/ttHTobb_ttToSemiLep_TTToSemiLeptonic_2018.h5"
+output_file="/eos/user/m/mmarcheg/ttHbb/training_datasets/classifier/multiclassifier_2018/ttHTobb_ttToSemiLep_TTToSemiLeptonic_2018_reweighed.h5"
 
 # Construct the command using the list variable
-command="python scripts/dataset/parquet_to_h5.py --cfg parameters/features_spanet.yaml -i ${input_files[*]} -o $output_file"
+command="python scripts/dataset/parquet_to_h5.py --cfg parameters/features_spanet_ttbb.yaml -i ${input_files[*]} -o $output_file --reweigh"
 
 # Run the command
-$command
+#$command
 
 input_files=(
     "/eos/user/m/mmarcheg/ttHbb/training_datasets/2018/output_ttHTobb_ttToSemiLep_2018.parquet"
@@ -23,8 +23,22 @@ input_files=(
     "/eos/user/m/mmarcheg/ttHbb/training_datasets/2018/output_TTbbSemiLeptonic_2018_TTbbSemiLeptonic__TTbbSemiLeptonic_tt+LF.parquet"
 )
 
-output_file="/eos/user/m/mmarcheg/ttHbb/training_datasets/classifier/multiclassifier_2018/ttHTobb_ttToSemiLep_TTbbSemiLeptonic_2018.h5"
+output_file="/eos/user/m/mmarcheg/ttHbb/training_datasets/classifier/multiclassifier_2018/ttHTobb_ttToSemiLep_TTbbSemiLeptonic_2018_reweighed.h5"
 
-command="python scripts/dataset/parquet_to_h5.py --cfg parameters/features_spanet.yaml -i ${input_files[*]} -o $output_file"
+command="python scripts/dataset/parquet_to_h5.py --cfg parameters/features_spanet_ttbb.yaml -i ${input_files[*]} -o $output_file --reweigh"
+
+#$command
+
+# Define the input files replacing tt+B from the inclusive sample to the ttbb one
+input_files=(
+    "/eos/user/m/mmarcheg/ttHbb/training_datasets/2018/output_ttHTobb_ttToSemiLep_2018.parquet"
+    "/eos/user/m/mmarcheg/ttHbb/training_datasets/2018/output_TTbbSemiLeptonic_2018_TTbbSemiLeptonic__TTbbSemiLeptonic_tt+B.parquet"
+    "/eos/user/m/mmarcheg/ttHbb/training_datasets/2018/output_TTToSemiLeptonic_2018_TTToSemiLeptonic__TTToSemiLeptonic_tt+C.parquet"
+    "/eos/user/m/mmarcheg/ttHbb/training_datasets/2018/output_TTToSemiLeptonic_2018_TTToSemiLeptonic__TTToSemiLeptonic_tt+LF.parquet"
+)
+
+output_file="/eos/user/m/mmarcheg/ttHbb/training_datasets/classifier/multiclassifier_2018/tthbb_ttbar_with_ttbb_merging_2018.h5"
+
+command="python scripts/dataset/parquet_to_h5.py --cfg parameters/features_spanet_ttbb.yaml -i ${input_files[*]} -o $output_file"
 
 $command

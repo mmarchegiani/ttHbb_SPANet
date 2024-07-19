4e22815c4b6868206c8e0912545581d4845bba50
Matteo Marchegiani <matmarcheg@gmail.com>
2024-07-19 16:34:41 +0200

    Submission script with events balance

---
+++ scripts/submit_multiclassifier_full_Run2_8M_balance_events.sh
@@ -0,0 +1,2 @@
+python scripts/submit_to_condor.py --cfg jobs/config/classification_800epochs.yaml -of options_files/ttHbb_semileptonic/classifier/options_multiclassifier_full_Run2_8M_balance_events.json -l /eos/user/m/mmarcheg/ttHbb/models/multiclassifier_full_Run2_8M_balance_events
+python scripts/submit_to_condor.py --cfg jobs/config/classification_800epochs.yaml -of options_files/ttHbb_semileptonic/classifier/options_multiclassifier_full_Run2_btag_LMH_8M_balance_events.json -l /eos/user/m/mmarcheg/ttHbb/models/multiclassifier_full_Run2_btag_LMH_8M_balance_events

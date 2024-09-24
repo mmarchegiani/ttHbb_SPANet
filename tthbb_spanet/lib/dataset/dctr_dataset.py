2ebf9f6afb03e923d1125861cdfceac8278ca676
Matteo Marchegiani <matmarcheg@gmail.com>
2024-09-24 18:05:35 +0200

    Apply 1D reweighting in dataset object

--- tthbb_spanet/lib/dataset/dctr_dataset.py
+++ tthbb_spanet/lib/dataset/dctr_dataset.py
@@ -1,9 +1,40 @@
 import os
+import numpy as np
 import awkward as ak
 
+import torch
+from sklearn.preprocessing import StandardScaler
+
 from .base import Dataset
+from ttbb_dctr.lib.data_preprocessing import get_njet_reweighting
+from ttbb_dctr.lib.quantile_transformer import WeightedQuantileTransformer
+from ttbb_dctr.lib.data_preprocessing import get_device, _stack_arrays
 
 class DCTRDataset(Dataset):
+    def __init__(self, input_file, cfg=None, shuffle=True, reweigh=False, entrystop=None, has_data=False, label=True):
+        super().__init__(input_file, cfg=cfg, shuffle=shuffle, reweigh=reweigh, entrystop=entrystop, has_data=has_data, label=label)
+        if (not "spanet_output" in self.df.fields) | ("BJetGood" not in self.df.fields):
+            self.create_branches()
+
+    def create_branches(self):
+        mask_btag = ak.values_astype(self.df.JetGood.btag_M, bool)
+        self.df = ak.with_field(self.df, self.df.JetGood[mask_btag], "BJetGood")
+        transformer = WeightedQuantileTransformer(n_quantiles=100000, output_distribution='uniform')
+        mask_tthbb = self.df.tthbb == 1
+        X = self.df.spanet_output.tthbb[mask_tthbb]
+        transformer.fit(X, sample_weight=-self.df.event.weight[mask_tthbb]) # Fit quantile transformer on ttHbb sample only (- in front of weights due to negative weights in DCTR samples)
+        transformed_score = transformer.transform(self.df.spanet_output.tthbb)
+        self.df["spanet_output"] = ak.with_field(self.df.spanet_output, transformed_score, "tthbb_transformed")
+        # Create a copy of the original weights
+        self.df["event"] = ak.with_field(self.df.event, self.df.event.weight, "weight_original")
+
+    def compute_njet_weights(self):
+        '''Compute the 1D reweighting based on the number of jets.'''
+        mask_num = self.df.dctr == 1
+        mask_den = self.df.dctr == 0
+        for name, mask in self.masks.items():
+            self.store_weight(name, get_njet_reweighting(self.df, mask & mask_num, mask & mask_den))
+
     def check_output(self, output_file):
         '''Check the output file extension and if it already exists.'''
         # Check the output file extension
@@ -15,6 +46,11 @@ class DCTRDataset(Dataset):
             raise ValueError(f"Output file {output_file} already exists.")
         os.makedirs(os.path.abspath(os.path.dirname(output_file)), exist_ok=True)
 
+    def apply_weight(self, df, weight):
+        '''Apply the event weights.'''
+        df["event"] = ak.with_field(df["event"], df.event.weight * weight, "weight")
+        return df
+
     def save(self, output_file, mask_name=None):
         '''Save the parquet file.'''
         os.makedirs(os.path.abspath(os.path.dirname(output_file)), exist_ok=True)
@@ -22,6 +58,8 @@ class DCTRDataset(Dataset):
             df_to_save = getattr(self, dataset)
             if mask_name is not None:
                 mask = self.masks[mask_name][getattr(self, f"{dataset}_mask")]
+                if mask_name in self.weights.keys():
+                    df_to_save = self.apply_weight(df_to_save, self.weights[mask_name][getattr(self, f"{dataset}_mask")])
                 df_to_save = df_to_save[mask]
             output_file_dataset = output_file.replace(".parquet", f"_{mask_name}_{dataset}_{len(df_to_save)}.parquet")
             self.check_output(output_file_dataset)

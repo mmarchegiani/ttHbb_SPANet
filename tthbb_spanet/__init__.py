106cf06315a59b180927e702e3e73ccc615fc9f0
Matteo Marchegiani <matmarcheg@gmail.com>
2024-12-28 00:34:21 +0100

    Remove import from ttbb-dctr package

--- tthbb_spanet/__init__.py
+++ tthbb_spanet/__init__.py
@@ -1,4 +1,3 @@
 from tthbb_spanet.lib.dataset.base import Dataset
-from tthbb_spanet.lib.dataset.dctr_dataset import DCTRDataset
 from tthbb_spanet.lib.dataset.spanet_dataset import SPANetDataset
 from tthbb_spanet.lib.dataset.parquet import ParquetDataset

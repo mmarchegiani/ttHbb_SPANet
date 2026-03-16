8f9087489e556af30a0070636fd109d04e5707b2
Sena Durgut <abrekk01@gmail.com>
2026-03-16 20:47:34 +0100

    fixing import issue

--- tthbb_spanet/__init__.py
+++ tthbb_spanet/__init__.py
@@ -1,4 +1,10 @@
 from tthbb_spanet.lib.dataset.base import Dataset
-from tthbb_spanet.lib.dataset.parquet import ParquetDataset
 from tthbb_spanet.lib.dataset.spanet_dataset import SPANetDataset
 from tthbb_spanet.lib.dataset.dctr_dataset import DCTRDataset
+
+# ParquetDataset depends on coffea; keep it optional for scripts
+# that do not need parquet conversion.
+try:
+    from tthbb_spanet.lib.dataset.parquet import ParquetDataset
+except ModuleNotFoundError:
+    ParquetDataset = None
